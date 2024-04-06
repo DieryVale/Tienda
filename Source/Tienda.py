a3e3a4211b0802371b4a2c9d0b4f9e65faacb216
DieryVale <dieryvale.01@gmail.com>
2024-04-05 20:59:48 -0500

    producto

--- Source/Tienda.py
+++ Source/Tienda.py
@@ -1,16 +1,25 @@
+from Producto import Producto 
+
 class Tienda:
     '''---------------------------------------------------------------------------
     # Atributos 
     ------------------------------------------------------------------------------'''
-    __producto1 = None
+    ''' __producto1 = None
     __producto2 = None 
-    __producto3 = None 
+    __producto3 = None    # igual que en producto
     __producto4 = None 
-    __dineroEnCaja = 0.0
+    __dineroEnCaja = 0.0'''
 
     '''-----------------------------------------------------------------------------
     # Metodos
     --------------------------------------------------------------------------------'''
+    def __init__(self):
+        self.__producto1 = Producto("PAPELERIA", "LAPIZ", 500, 30, 9)
+        self.__producto2 = Producto("PAPELERIA", "BORRADOR", 300, 15, 5)
+        self.__producto3 = Producto("SUPERMERCADO", "CAFE", 5600, 20, 10)
+        self.__producto4 = Producto("DROGUERIA", "DESINFECTANTE", 3200, 12, 11)
+        self.__dineroEnCaja = 0.0
+
 
     def getProducto1(self):
         return self.__producto1
